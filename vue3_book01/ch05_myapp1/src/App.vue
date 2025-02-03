c97c9ac4b686ccda54a508e16d5c757f3f8c3f6b
gimeast <dudu.gimeast@gmail.com>
2025-02-03 20:52:14 +0900

    기본적인 스타일 적용 예제

---
+++ vue3_book01/ch05_myapp1/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <BoardList />
+  </div>
+</template>
+
+<script>
+import BoardList from './components/BoardList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    BoardList,
+  },
+}
+</script>
+
+<style></style>
