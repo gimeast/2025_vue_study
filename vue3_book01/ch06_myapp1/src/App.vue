aef178489a158f8d18c062f31b12b6da75bc51e9
gimeast <dudu.gimeast@gmail.com>
2025-02-04 15:38:03 +0900

    npm init을 활용한 날씨 위젯

---
+++ vue3_book01/ch06_myapp1/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container mt-5">
+    <h1 class="text-center mb-4">날씨 위젯</h1>
+    <WeatherWidget />
+  </div>
+</template>
+
+<script setup>
+import WeatherWidget from './components/WeatherWidget.vue'
+</script>
+
+<style></style>
